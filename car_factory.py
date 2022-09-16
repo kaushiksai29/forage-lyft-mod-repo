6e68397f24694cb10d45bd089edde64c6639c505
kaushiksai29 <51222741+kaushiksai29@users.noreply.github.com>
2022-09-15 17:49:59 -0700

    Add files via upload

--- car_factory.py
+++ car_factory.py
@@ -1,9 +1,6 @@
-import imp
-import battery
 from car import Car
 from battery.nubbin_battery import NubbinBattery
 from battery.spindler_battery import SpindlerBattery
-import engine
 from engine.capulet_engine import CapuletEngine
 from engine.sternman_engine import SternmanEngine
 from engine.willoughby_engine import WilloughbyEngine
